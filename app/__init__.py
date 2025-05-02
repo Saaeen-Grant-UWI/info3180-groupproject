cfb30a22ff6e9d4389f520434aa037e4d18cbfd8
Saaeen-Grant-UWI <saaeengrant.uwi@gmail.com>
2025-05-02 00:00:11 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
-from flask import Flask 
-
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/jamdate'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'secretckey'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/jamdate'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'secretckey'
 
-from app import models, routes
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    from app import routes, models
+    return app
